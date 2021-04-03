e64216708f348b8b4315d31b2afcd2ed9e20f7d5
George Hotz <geohot@gmail.com>
2021-04-02 18:54:27 -0700

    static build

---
+++ build/precache-manifest.d88638cbfc82bab4cd7c9274c151709d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5880f66b9c2d059daf3798f4610c1035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6619a6b9823ee4be16aa",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "f5224b0a57c807f1978e",
+    "url": "/static/js/2.661ff176.chunk.js"
+  },
+  {
+    "revision": "aaf168b94062fd5eddc2bafce074e7d6",
+    "url": "/static/js/2.661ff176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6619a6b9823ee4be16aa",
+    "url": "/static/js/main.cc56ffe0.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  }
+]);
